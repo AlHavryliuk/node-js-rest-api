32b31e5ac79d143d282a5cfbcce5f43c42525403
AlekasndrHavryliuk <104818533+AlekasndrHavryliuk@users.noreply.github.com>
2023-04-19 19:41:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ import mongoose from "mongoose";
 import app from "./app.js";
 import { DB_HOST } from "./configuration/config.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const HOST = DB_HOST ?? process.env.DB_HOST;
 
 mongoose
-  .connect(DB_HOST)
-  .then(() => console.log("Database connection successful"))
-  .catch((err) => console.log(err.message));
+  .connect(HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1)
+  });
