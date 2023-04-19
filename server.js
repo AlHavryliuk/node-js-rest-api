a761086ecbe3ac4d8bc5ac5dda15482b627ea318
AlekasndrHavryliuk <104818533+AlekasndrHavryliuk@users.noreply.github.com>
2023-04-19 20:14:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
-import app from "./app.js";
+// import app from "./app.js";
 
-const HOST = process.env.DB_HOST;
+// const HOST = process.env.DB_HOST;
+const DB_HOST =
+  "mongodb+srv://havryliuk:jxvZCQfm2Fa5gZR2@cluster0.5fdb83j.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 mongoose
-  .connect(HOST)
+  .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
-    });
+    // app.listen(3000, () => {
+    //   console.log("Server running. Use our API on port: 3000");
+    // });
   })
   .catch((err) => {
     console.log(err.message);
-    process.exit(1)
+    // process.exit(1)
   });
